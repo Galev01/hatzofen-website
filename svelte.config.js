1883457b07ba7ac1952de5b89c30bade27969f3c
Gal Lev <gallev01@gmail.com>
2025-05-12 14:55:55 +0300

    Update Svelte configuration to use adapter-node; add new dependencies in package.json and package-lock.json for improved deployment and functionality

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,26 @@
-import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
+
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are often sufficient for GitHub Pages
-			pages: 'docs',
-			assets: 'docs',
-			fallback: null, // can be 'index.html' or '200.html' for SPA-like behavior
-			precompress: false,
-			strict: true
-		})
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		// Use adapter-node now
+		adapter: adapter() 
+		// If using adapter-static previously:
+		// adapter: adapter({
+		// 	// default options are shown. On some platforms
+		// 	// these options are set automatically — see TBD
+		// 	pages: 'build',
+		// 	assets: 'build',
+		// 	fallback: undefined, // or 'index.html' or '200.html' for SPAs
+		// 	precompress: false,
+		// 	strict: true
+		// })
 	}
 };
 
